3f35d3de11888b2b10e6d3e8e6f2079fbc796d3f
Berkaoui Mohamed <berkaouimed1@gmail.com>
2026-07-21 20:59:54 +0100

    IDK

--- src/context/TransactionsStore.jsx
+++ src/context/TransactionsStore.jsx
@@ -9,20 +9,20 @@ function TransactionsStore({ children }) {
    * @todo convert state to reducer {useReducer}
    */
 
-  function addNewTransaction(newtransaction) {
-    setTransactions([...transactions, newtransaction]);
-  }
-  function removeTransaction(transaction) {
-    setTransactions(
-      transactions.filter((item) => item.title != transaction.title),
-    );
-  }
-  function editTransaction(transaction) {
-    setTransactions(
-      transactions.map((item) =>
-        item.title == transaction ? transaction : item,
-      ),
-    );
+    function addNewTransaction(newtransaction) {
+      setTransactions([...transactions, newtransaction]);
+    }
+    function removeTransaction(transaction) {
+      setTransactions(
+        transactions.filter((item) => item.title != transaction.title),
+      );
+    }
+    function editTransaction(transaction) {
+      setTransactions(
+        transactions.map((item) =>
+          item.title == transaction ? transaction : item,
+        ),
+      );
   }
   return (
     <TransactionsContext.Provider value={{ transactions, addNewTransaction,editTransaction,removeTransaction }}>
